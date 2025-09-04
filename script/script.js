a221ac80242a6031595dd76010e23c22208b6123
Sai-S-Mc <sai.satti97@gmail.com>
2025-09-04 19:27:29 -0400

    Update script.js - project display spacing based on VW

--- script/script.js
+++ script/script.js
@@ -1,7 +1,23 @@
-function activateHamburger(){
-document.querySelector(".hamburger").classList.toggle("active");
-document.querySelector(".nav-menu").classList.toggle("active");
+function toggleClassBasedOnViewportWidth() {
+  if (window.innerWidth < breakpoint) {
+    projectContainerElement.classList.remove("container");
+  } else {
+    projectContainerElement.classList.add("container");
+  }
 }
 
+function activateHamburger() {
+  document.querySelector(".hamburger").classList.toggle("active");
+  document.querySelector(".nav-menu").classList.toggle("active");
+}
+
+let projectContainerElement = document.querySelector("#projects-display");
+let breakpoint = 768.5;
+
+toggleClassBasedOnViewportWidth();
+
 let hamburgerElement = document.querySelector(".hamburger");
-hamburgerElement.addEventListener("click",activateHamburger);
+hamburgerElement.addEventListener("click", activateHamburger);
+
+
+window.addEventListener("resize", toggleClassBasedOnViewportWidth)
