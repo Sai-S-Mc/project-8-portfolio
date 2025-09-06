db69d966b8664f04d27678b92a58de23f8b67cfe
Sai-S-Mc <sai.satti97@gmail.com>
2025-09-05 21:33:36 -0400

    Update script.js - footer span text spacing based on VW

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
 function toggleClassBasedOnViewportWidth() {
   if (projectContainerElement) {
-    if (window.innerWidth < breakpoint) {
+    if (window.innerWidth < 768.5) {
       projectContainerElement.classList.remove("container");
     } else {
       projectContainerElement.classList.add("container");
+    }
+  }
+
+  if (footerSpanElement) {
+    if (window.innerWidth < 991) {
+      footerSpanElement.classList.replace("ps-2", "ps-1");
+    } else {
+      footerSpanElement.classList.replace("ps-1","ps-2");
     }
   }
 }
@@ -16,6 +24,9 @@ function activateHamburger() {
 let projectContainerElement = document.querySelector("#projects-display");
 let breakpoint = 768.5;
 
+let footerSpanElement = document.querySelector(
+  ".contact-me-box span.text-muted"
+);
 toggleClassBasedOnViewportWidth();
 
 let hamburgerElement = document.querySelector(".hamburger");
